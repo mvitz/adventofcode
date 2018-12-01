7b0e649678d9cb66a3cb07c8af22c3771dee6bd2
Michael Vitz <michael.vitz@innoq.com>
2018-12-01 19:02:49 +0100

    2018/01/2: Set is faster

--- 2018/1/1.rb
+++ 2018/1/1.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 
-# Part 1
-frequency = File.readlines('input.txt', mode: 'r')
-  .map(&:to_i)
-  .reduce(:+)
+changes = File.readlines('input.txt', mode: 'r').map(&:to_i)
 
+# Part 1
+frequency = changes.reduce(:+)
 puts "Part 1: #{frequency}"
 
 # Part 2
-frequencies = [0]
+require 'set'
+
+frequency = 0
+frequencies = Set[0]
 duplicated_frequency = nil
 
 until duplicated_frequency
-  File.readlines('input.txt', mode: 'r')
-    .map(&:to_i)
-    .each do |change|
-      frequency = frequencies.last + change
-      unless duplicated_frequency
-        duplicated_frequency = frequency if frequencies.include? frequency
-        frequencies << frequency
-      end
+  changes.each do |change|
+    frequency = frequency + change
+    unless frequencies.add? frequency
+      duplicated_frequency = frequency
+      break
     end
+  end
 end
 
 puts "Part 2: #{duplicated_frequency}"
