c15fdddbdade645f8ab9562deb8e1da95ec08e74
Michael Vitz <michael.vitz@innoq.com>
2018-12-01 17:41:19 +0100

    2018/01: Ruby solutions for part 1 and 2

---
+++ 2018/1/1.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+# Part 1
+frequency = 0
+
+File.open('input.txt', 'r') do |f|
+  f.each_line do |change|
+    frequency = frequency + change.to_i
+  end
+end
+
+puts "Part 1: #{frequency}"
+
+# Part 2
+frequencies = [0]
+duplicated_frequency = nil
+
+until duplicated_frequency
+  File.open('input.txt', 'r') do |f|
+    f.each_line do |change|
+      frequency = frequencies.last + change.to_i
+      unless duplicated_frequency
+        duplicated_frequency = frequency if frequencies.include? frequency
+        frequencies << frequency
+      end
+    end
+  end
+end
+
+puts "First duplicated frequency: #{duplicated_frequency}"
