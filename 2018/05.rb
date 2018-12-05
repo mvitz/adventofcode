a1cf43655486a1bb2b245fbafa826671d501933a
Michael Vitz <michael.vitz@innoq.com>
2018-12-06 00:37:22 +0100

    2018/05/2: Some more refactoring

--- 2018/05.rb
+++ 2018/05.rb
@@ -10,14 +10,17 @@ class String
   def swapcase
     if self.is_upcase? then self.downcase else self.upcase end
   end
+
+  def remove_units(*units)
+    self.chars.reject { |unit| units.include? unit }.join('')
+  end
 end
 
 def react(polymer)
   polymer.chars.reduce([]) do |result, unit|
     last_unit = result.last
-    current = unit.swapcase
 
-    if last_unit == current
+    if last_unit == unit.swapcase
       result.pop
     else
       result << unit
@@ -35,7 +38,7 @@ puts reacted_polymer.length
 
 # Part 2
 polymers = ('a'..'z').map do |unit|
-  polymer.gsub(Regexp.new("[#{unit}#{unit.upcase}]"), '')
+  polymer.remove_units(unit, unit.upcase)
 end
 
 reacted_polymers = polymers.map do |polymer|
