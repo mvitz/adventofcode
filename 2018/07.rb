204c3ff7f976b85e06793a230df63436bbe05906
Michael Vitz <michael.vitz@innoq.com>
2018-12-07 11:27:08 +0100

    2018/07/2: Initial solution

--- 2018/07.rb
+++ 2018/07.rb
@@ -44,3 +44,62 @@ end
 
 puts solution
 
+# Part 2
+steps = Hash.new { |hash, key| hash[key] = [] }
+instructions.each do |instruction|
+  match = /^Step (\w) must be finished before step (\w) can begin.$/.match instruction
+
+  step = match[2]
+  requirement = match[1]
+
+  requirements = steps[step]
+  requirements << requirement
+  steps[step] = requirements
+
+  steps[requirement] = steps[requirement]
+end
+
+step_durations = ('A'..'Z').each_with_index.map { |v, i| [v, 61 + i] }.to_h
+#step_durations = ('A'..'Z').each_with_index.map { |v, i| [v,  1 + i] }.to_h
+work = {}
+workers = 5
+#workers = 2
+duration = 0
+
+while steps.count > 0 or work.count > 0 do
+#4.times do
+  #puts "Second #{duration}"
+
+  finished_steps = work.delete(0) || []
+  #puts "finished_steps=#{finished_steps.inspect}"
+  finished_steps.each do |step|
+    steps.transform_values! do |requirements|
+      requirements.delete step
+      requirements
+    end
+  end
+  #puts "steps=#{steps.inspect}"
+  workers += finished_steps.length
+  #puts "workers=#{workers}"
+
+  next_steps = steps.select { |step, requirement| requirement.empty? }.keys.sort.take workers
+  #puts "next_steps=#{next_steps.inspect}"
+  next_steps.each { |step| steps.delete step }
+  workers -= next_steps.length
+  #puts "workers=#{workers}"
+
+  next_steps.each do |step|
+    step_duration = step_durations[step]
+    work_for_duration = work[step_duration] || []
+    work_for_duration << step
+    work[step_duration] = work_for_duration
+  end
+  #puts "work=#{work.inspect}"
+
+  work.transform_keys! { |remaining_duration| remaining_duration - 1 }
+  #puts "work=#{work.inspect}"
+
+  duration += 1
+end
+
+puts duration - 1
