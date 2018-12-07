0bfd70b6fe4a69c3c08b5ae46d2eb56a9b984e0e
Michael Vitz <michael.vitz@innoq.com>
2018-12-07 11:56:32 +0100

    2018/07: Extract another method

--- 2018/07.rb
+++ 2018/07.rb
@@ -19,6 +19,43 @@ def read_steps(instructions)
   end
 end
 
+def process_steps(steps, workers = 1, step_durations = Hash.new(1))
+  duration = -1
+
+  steps_in_progress = {}
+  finished_steps = []
+
+  while steps.count > 0 or steps_in_progress.count > 0 do
+
+    turn_finished_steps = steps_in_progress.delete(0) || []
+    turn_finished_steps.each do |step|
+      steps.transform_values! do |requirements|
+        requirements.delete step
+        requirements
+      end
+    end
+    workers += turn_finished_steps.length
+    finished_steps += turn_finished_steps
+
+    next_steps = steps.select { |step, requirement| requirement.empty? }.keys.sort.take workers
+    next_steps.each { |step| steps.delete step }
+    workers -= next_steps.length
+
+    next_steps.each do |step|
+      step_duration = step_durations[step]
+      work_for_duration = steps_in_progress[step_duration] || []
+      work_for_duration << step
+      steps_in_progress[step_duration] = work_for_duration
+    end
+
+    steps_in_progress.transform_keys! { |remaining_duration| remaining_duration - 1 }
+
+    duration += 1
+  end
+
+  [finished_steps.join(''), duration]
+end
+
 instructions = [
   'Step C must be finished before step A can begin.',
   'Step C must be finished before step F can begin.',
@@ -32,65 +69,16 @@ instructions = File.readlines('07_input.txt').map(&:strip)
 
 # Part 1
 steps = read_steps(instructions)
-solution = ''
-while steps.count > 0 do
-  next_steps = steps.select { |step, requirement| requirement.empty? }.keys
-  next_step = next_steps.sort.first
-
-  steps.delete next_step
-  steps.transform_values! do |requirements|
-    requirements.delete next_step
-    requirements
-  end
-
-  solution += next_step
-end
-
-puts solution
+(order, _) = process_steps(steps)
+puts order
 
 # Part 2
-steps = read_steps(instructions)
-step_durations = ('A'..'Z').each_with_index.map { |v, i| [v, 61 + i] }.to_h
-#step_durations = ('A'..'Z').each_with_index.map { |v, i| [v,  1 + i] }.to_h
-work = {}
 workers = 5
 #workers = 2
-duration = 0
 
-while steps.count > 0 or work.count > 0 do
-#4.times do
-  #puts "Second #{duration}"
+step_durations = ('A'..'Z').each_with_index.map { |v, i| [v, 61 + i] }.to_h
+#step_durations = ('A'..'Z').each_with_index.map { |v, i| [v,  1 + i] }.to_h
 
-  finished_steps = work.delete(0) || []
-  #puts "finished_steps=#{finished_steps.inspect}"
-  finished_steps.each do |step|
-    steps.transform_values! do |requirements|
-      requirements.delete step
-      requirements
-    end
-  end
-  #puts "steps=#{steps.inspect}"
-  workers += finished_steps.length
-  #puts "workers=#{workers}"
-
-  next_steps = steps.select { |step, requirement| requirement.empty? }.keys.sort.take workers
-  #puts "next_steps=#{next_steps.inspect}"
-  next_steps.each { |step| steps.delete step }
-  workers -= next_steps.length
-  #puts "workers=#{workers}"
-
-  next_steps.each do |step|
-    step_duration = step_durations[step]
-    work_for_duration = work[step_duration] || []
-    work_for_duration << step
-    work[step_duration] = work_for_duration
-  end
-  #puts "work=#{work.inspect}"
-
-  work.transform_keys! { |remaining_duration| remaining_duration - 1 }
-  #puts "work=#{work.inspect}"
-
-  duration += 1
-end
-
-puts duration - 1
+steps = read_steps(instructions)
+(_, duration) = process_steps(steps, workers, step_durations)
+puts duration
