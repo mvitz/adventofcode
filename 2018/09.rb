83e39d941ef263f48bb6a8837596cc5b6e4b56e4
Michael Vitz <michael.vitz@innoq.com>
2018-12-09 16:51:36 +0100

    2018/09/1: Extract method

--- 2018/09.rb
+++ 2018/09.rb
@@ -6,23 +6,29 @@
 
 players = 486
 last_marble = 70833
-current_marble = 0
-points = players.times.map {|x| 0 }
-marbles = [0]
 
-# Part 1
-(1..last_marble).each do |marble|
-  if marble % 23 == 0
-    current_player = marble % players
-    points[current_player] += marble
-    current_marble = current_marble - 7
-    current_marble = marbles.length + current_marble if current_marble < 0
-    points[current_player] += marbles.delete_at current_marble
-  else
-    current_marble = current_marble + 2
-    current_marble -= marbles.length if current_marble > marbles.length
-    marbles.insert(current_marble, marble)
+def play(players, last_marble)
+  points = players.times.map {|x| 0 }
+
+  marbles = [0]
+  current_marble = 0
+
+  (1..last_marble).each do |marble|
+    if marble % 23 == 0
+      current_player = marble % players
+      points[current_player] += marble
+      current_marble = current_marble - 7
+      current_marble = marbles.length + current_marble if current_marble < 0
+      points[current_player] += marbles.delete_at current_marble
+    else
+      current_marble = current_marble + 2
+      current_marble -= marbles.length if current_marble > marbles.length
+      marbles.insert(current_marble, marble)
+    end
   end
+
+  points
 end
 
-puts points.reduce { |highscore, score| if highscore > score then highscore else score end }
+# Part 1
+puts play(players, last_marble).reduce { |highscore, score| if highscore > score then highscore else score end }
