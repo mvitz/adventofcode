218b7b7ade6a4feb7a61ab1ef17596460b6768f5
Michael Vitz <michael.vitz@innoq.com>
2018-12-09 16:48:31 +0100

    2018/09/1: Initial solution

---
+++ 2018/09.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+#
+# Solution for https://adventofcode.com/2018/day/9
+
+# 486 players; last marble is worth 70833 points
+
+players = 486
+last_marble = 70833
+current_marble = 0
+points = players.times.map {|x| 0 }
+marbles = [0]
+
+# Part 1
+(1..last_marble).each do |marble|
+  if marble % 23 == 0
+    current_player = marble % players
+    points[current_player] += marble
+    current_marble = current_marble - 7
+    current_marble = marbles.length + current_marble if current_marble < 0
+    points[current_player] += marbles.delete_at current_marble
+  else
+    current_marble = current_marble + 2
+    current_marble -= marbles.length if current_marble > marbles.length
+    marbles.insert(current_marble, marble)
+  end
+end
+
+puts points.reduce { |highscore, score| if highscore > score then highscore else score end }
