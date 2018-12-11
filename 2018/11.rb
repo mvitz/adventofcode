a2f69bdebd37d2604f07506130d14ad3edfa5b22
Michael Vitz <michael.vitz@innoq.com>
2018-12-11 22:26:58 +0100

    2018/11/2: Initial solution

--- 2018/11.rb
+++ 2018/11.rb
@@ -17,30 +17,51 @@ end
 #puts power_level(39, [217, 196])
 #puts power_level(71, [101, 153])
 
-def largest_total_power_grid(serial_number)
-  grid = (1..300).map do |y|
-    (1..300).map do |x|
+def grid(serial_number, size = 300)
+  (1..size).map do |y|
+    (1..size).map do |x|
       power_level(serial_number, [x, y])
     end
   end
+end
 
-  power_to_grid = []
-  (0..297).each do |y|
-    (0..297).each do |x|
-      power = 3.times.map { |vx|
-        3.times.map do |vy|
-          grid[y + vy][x + vx]
+def power_of_grid(grid, x, y, size, cache)
+  if size == 1
+    return grid[y][x]
+  end
+
+  power = cache[[x, y, size - 1]]
+  unless power
+    power = power_of_grid(grid, x, y, size - 1, cache)
+  end
+  (size - 1).times do |dy|
+    power += grid[y + dy][x + size - 1]
+  end
+  power += grid[y + size - 1][x..(x + size - 1)].reduce(:+)
+  power
+end
+
+def largest_power_grid(grid, min_size = 1, max_size = 300)
+  grids_to_power = {}
+  grid.each_with_index do |line, y|
+    for x in 0 ... line.size
+      min_size.upto(max_size).each do |size|
+        if (x + size) <= line.size and (y + size) <= grid.size
+          power = power_of_grid(grid, x, y, size, grids_to_power)
+          grids_to_power[[x, y, size]] = power
         end
-      }.flatten.reduce(:+)
-      power_to_grid << [power, [x + 1, y + 1]]
+      end
     end
   end
 
-  power_to_grid.sort_by { |item| item.first }.last
+  result = grids_to_power.sort_by { |k,v| v }.last.first
+  "#{result[0] + 1},#{result[1] + 1},#{result[2]}"
 end
 
-#puts largest_total_power_grid(18).inspect
-#puts largest_total_power_grid(42).inspect
-
 # Part 1
-puts largest_total_power_grid(9221).inspect
+puts largest_power_grid(grid(9221), 3, 3)
+
+# Part 2
+#puts largest_power_grid(grid(18))
+#puts largest_power_grid(grid(42))
+puts largest_power_grid(grid(9221))
