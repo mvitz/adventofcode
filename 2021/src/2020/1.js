5e1cee5c7065cba698063fa886a8655b2e134dfa
Michael Vitz <vitz.michael@googlemail.com>
2021-11-29 17:03:40 +0100

    Solve 2020-01 part 2, too

--- 2021/src/2020/1.js
+++ 2021/src/2020/1.js
@@ -1,15 +1,28 @@
-const fixExpenseReport = (expenseReport) => {
-  for (let i = 0; i < expenseReport.length; i++) {
-    const firstEntry = expenseReport[i]
-    for (let j = i + 1; j < expenseReport.length; j++) {
-      const secondEntry = expenseReport[j]
-      if (firstEntry + secondEntry === 2020) {
-        return firstEntry * secondEntry;
-      }
-    }
-  }
-
-  throw new Error(`Unable to fix the given expense report: ${expenseReport}`)
+const fixExpenseReport = (expenseReport, size) => {
+  return combinations(expenseReport, size)
+    .filter(combination => sum(combination) === 2020)
+    .map(combination => product(combination))[0]
 }
 
-module.exports = fixExpenseReport
+const combinations = (candidates, size, entry = []) => {
+  if (size === 0) {
+    return [entry]
+  }
+
+  const result = []
+  for (let i = 0; i < candidates.length; i++) {
+    const newEntry = [...entry, candidates[i]]
+    result.push(...combinations(candidates.slice(i + 1), size - 1, newEntry))
+  }
+  return result
+}
+
+const sum = (array) => {
+  return array.reduce((accumulator, entry) => accumulator + entry, 0)
+}
+
+const product = (array) => {
+  return array.reduce((accumulator, entry) => accumulator * entry, 1)
+}
+
+module.exports = { fixExpenseReport }
