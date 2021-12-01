b565fe785cc8394c5a48a8014d61efba39afeae3
Michael Vitz <vitz.michael@googlemail.com>
2021-12-01 12:05:07 +0100

    Refactoring

--- 2021/src/2021/1.js
+++ 2021/src/2021/1.js
@@ -6,19 +6,47 @@ const part2 = report => {
   return solve(report, 3)
 }
 
+const DIRECTION = Object.freeze({
+  DOWN: 'DOWN',
+  STEADY: 'STEADY',
+  UP: 'UP'
+})
+
 const solve = (report, windowSize) => {
-  return windows(report, windowSize)
+  const windows = toWindows(report, windowSize)
     .map(window => windowSum(window))
-    .reduce((result, depth) => {
-      return {
-        increased: depth > result.lastDepth ? result.increased + 1 : result.increased,
-        lastDepth: depth
-      }
-    }, { increased: 0, lastDepth: Number.POSITIVE_INFINITY })
-    .increased
+
+  return analyze(windows)
+    .filter(entry => entry === DIRECTION.DOWN)
+    .length
 }
 
-const windows = (report, windowSize) => {
+const analyze = entries => {
+  const result = []
+
+  for (let i = 1; i < entries.length; i++) {
+    const current = entries[i]
+    const previous = entries[i - 1]
+
+    const direction = toDirection(current, previous)
+
+    result.push(direction)
+  }
+
+  return result
+}
+
+const toDirection = (current, last) => {
+  if (current > last) {
+    return DIRECTION.DOWN
+  } else if (current < last) {
+    return DIRECTION.UP
+  } else {
+    return DIRECTION.STEADY
+  }
+}
+
+const toWindows = (report, windowSize) => {
   const result = []
   for (let i = 0; i < report.length + 1 - windowSize; i++) {
     const window = []
@@ -34,4 +62,4 @@ const windowSum = window => {
   return window.reduce((sum, number) => sum + number, 0)
 }
 
-module.exports = { part1, part2, windows }
+module.exports = { part1, part2, windows: toWindows }
