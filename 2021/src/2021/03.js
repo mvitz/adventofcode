4752f3debfd3231d1e5ce7b5877e01f97b065cf4
Michael Vitz <vitz.michael@googlemail.com>
2021-12-03 10:46:30 +0100

    Refactoring

--- 2021/src/2021/03.js
+++ 2021/src/2021/03.js
@@ -2,76 +2,80 @@ const part1 = report => {
   return gammaRateOf(report) * epsilonRateOf(report)
 }
 
-const part2 = report => {
-  return oxygenGeneratorRatingOf(report) * co2ScrubberRating(report)
-}
-
-const oxygenGeneratorRatingOf = report => {
-  let positions = report
-  let round = 0
-  while (positions.length > 1) {
-    positions = analyze2(positions, mostCommonBitInPosition, round++)
-  }
-
-  return parseInt(positions[0], 2)
-}
-
-const co2ScrubberRating = report => {
-  let positions = report
-  let round = 0
-  while (positions.length > 1) {
-    positions = analyze2(positions, leastCommonBitInPosition, round++)
-  }
-
-  return parseInt(positions[0], 2)
-}
-
-const analyze2 = (report, strategy, i = 0) => {
-  const bar = strategy(report, i)
-  return report.map(toBits)
-    .filter(bits => bits[i] === bar)
-    .map(toBinaryNumber)
-}
-
-const mostCommonBitInPosition = (report, position) => {
-  const bitsInPosition = analyse(report)[position]
-    .flatMap(bits => bits)
-  return bitsInPosition.filter(bit => bit === 1).length >= report.length / 2 ? 1 : 0
-}
-
-const leastCommonBitInPosition = (report, position) => {
-  const bitsInPosition = analyse(report)[position]
-    .flatMap(bits => bits)
-  return bitsInPosition.filter(bit => bit === 1).length < report.length / 2 ? 1 : 0
-}
-
 const gammaRateOf = report => {
-  const gammaRateBits = analyse(report)
-    .map(bitsInPosition => {
-      return bitsInPosition.filter(bit => bit === 1).length > report.length / 2 ? 1 : 0
-    })
-    .reduce((bits, mostCommonBit) => bits + mostCommonBit, '')
-
-  return parseInt(gammaRateBits, 2)
+  return analyze(report, mostCommonBitIn)
 }
 
 const epsilonRateOf = report => {
-  const epsilonRateBits = analyse(report)
-    .map(bitsInPosition => {
-      return bitsInPosition.filter(bit => bit === 1).length < report.length / 2 ? 1 : 0
-    })
-    .reduce((bits, mostCommonBit) => bits + mostCommonBit, '')
-
-  return parseInt(epsilonRateBits, 2)
+  return analyze(report, leastCommonBitIn)
 }
 
-const analyse = report => {
-  return report
+const analyze = (numbers, strategy) => {
+  const bits = groupBitsByPosition(numbers)
+    .map(strategy)
+  return toNumber(toBinaryNumber(bits))
+}
+
+const part2 = report => {
+  return oxygenGeneratorRatingOf(report) * co2ScrubberRatingOf(report)
+}
+
+const oxygenGeneratorRatingOf = report => {
+  return analyze2(report, mostCommonBitInPosition)
+}
+
+const co2ScrubberRatingOf = report => {
+  return analyze2(report, leastCommonBitInPosition)
+}
+
+const analyze2 = (numbers, strategy, position = 0) => {
+  if (numbers.length === 1) {
+    return toNumber(numbers[0])
+  }
+
+  const bitResultInPosition = strategy(numbers, position)
+  const remainingNumbers = numbers
     .map(toBits)
-    .reduce((result, bits) => {
-      bits.forEach((bit, i) => (result[i] = result[i] || []).push(bit))
-      return result
+    .filter(bits => bits[position] === bitResultInPosition)
+    .map(toBinaryNumber)
+  return analyze2(remainingNumbers, strategy, position + 1)
+}
+
+const mostCommonBitInPosition = (numbers, position) => {
+  const bitsInPosition = groupBitsByPosition(numbers)[position]
+  return mostCommonBitIn(bitsInPosition)
+}
+
+const leastCommonBitInPosition = (numbers, position) => {
+  const bitsInPosition = groupBitsByPosition(numbers)[position]
+  return leastCommonBitIn(bitsInPosition)
+}
+
+const groupBitsByPosition = numbers => {
+  return numbers
+    .map(toBits)
+    .reduce((bitsByPosition, bits) => {
+      bits.forEach((bit, position) => (bitsByPosition[position] = bitsByPosition[position] || []).push(bit))
+      return bitsByPosition
     }, [])
+}
+
+// returns an array that contains the number of 0 or 1 at the corresponding position
+const analyseBits = bits => {
+  return bits.reduce((statistic, bit) => {
+    statistic[bit]++
+    return statistic
+  }, [0, 0])
+}
+
+const mostCommonBitIn = bits => {
+  const [zeros, ones] = analyseBits(bits)
+  return ones >= zeros ? 1 : 0
+}
+
+const leastCommonBitIn = bits => {
+  const [zeros, ones] = analyseBits(bits)
+  return ones < zeros ? 1 : 0
 }
 
 const toBits = binaryNumber => {
@@ -82,6 +86,10 @@ const toBits = binaryNumber => {
 
 const toBinaryNumber = bits => {
   return bits.reduce((binaryNumber, bit) => binaryNumber + bit, '')
+}
+
+const toNumber = binaryNumber => {
+  return parseInt(binaryNumber, 2)
 }
 
 module.exports = { part1, part2 }
