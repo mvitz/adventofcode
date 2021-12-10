a48a6c0d6fe2606e5dac6451552112ae305e6ce2
Michael Vitz <vitz.michael@googlemail.com>
2021-12-10 08:31:46 +0100

    Solve 2021-10 part 2

--- 2021/src/2021/10.js
+++ 2021/src/2021/10.js
@@ -8,6 +8,18 @@ const part1 = navigationSubsystem => {
     .reduce(sum)
 }
 
+const part2 = navigationSubsystem => {
+  const incompleteLine = line => !findFirstIllegalCharacterIn(line)
+
+  const result = linesOf(navigationSubsystem)
+    .filter(incompleteLine)
+    .map(fixIncompleteLine)
+    .map(toFixScore)
+    .sort((a, b) => a - b)
+
+  return result[(result.length - 1) / 2]
+}
+
 const findFirstIllegalCharacterIn = line => {
   const stack = []
 
@@ -26,6 +38,29 @@ const findFirstIllegalCharacterIn = line => {
   return null
 }
 
+const fixIncompleteLine = line => {
+  const stack = []
+
+  for (const token of line.split('')) {
+    const openToken = ['(', '[', '{', '<'].findIndex(t => t === token)
+    if (openToken > -1) {
+      stack.push(openToken)
+    } else {
+      const closeToken = [')', ']', '}', '>'].findIndex(t => t === token)
+      if (closeToken > -1) {
+        stack.pop()
+      }
+    }
+  }
+
+  const fixingClosingTokens = []
+  for (const token of stack.reverse()) {
+    const matchingCloseToken = [')', ']', '}', '>'][token]
+    fixingClosingTokens.push(matchingCloseToken)
+  }
+  return fixingClosingTokens
+}
+
 const SYNTAX_ERROR_SCORES = Object.freeze({
   ')': 3,
   ']': 57,
@@ -35,6 +70,16 @@ const SYNTAX_ERROR_SCORES = Object.freeze({
 
 const toLineScore = firstIllegalCharacter => SYNTAX_ERROR_SCORES[firstIllegalCharacter]
 
+const FIX_CHARACTER_SCORE = Object.freeze({
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4
+})
+
+const toFixScore = characters =>
+  characters.reduce((score, character) => (score * 5) + FIX_CHARACTER_SCORE[character], 0)
+
 const sum = (result, number) => result + number
 
-module.exports = { part1 }
+module.exports = { part1, part2 }
--- 2021/tests/2021/10.test.js
+++ 2021/tests/2021/10.test.js
@@ -1,5 +1,5 @@
 const { readPuzzleInput } = require('../../src/utils')
-const { part1 } = require('../../src/2021/10')
+const { part1, part2 } = require('../../src/2021/10')
 
 describe('2021 day 10', () => {
   const example = `\
@@ -29,6 +29,24 @@ describe('2021 day 10', () => {
       const result = part1(input)
 
       expect(result).toBe(392367)
+    })
+  })
+
+  describe('part 2', () => {
+    it('should solve example', () => {
+      const input = example
+
+      const result = part2(input)
+
+      expect(result).toBe(288957)
+    })
+
+    it('should solve my input', () => {
+      const input = readPuzzleInput(2021, 10)
+
+      const result = part2(input)
+
+      expect(result).toBe(2192104158)
     })
   })
 })
