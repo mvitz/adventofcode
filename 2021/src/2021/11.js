d07ec2e88382f2444653a7a00b1944608eaa01e7
Michael Vitz <vitz.michael@googlemail.com>
2021-12-11 13:50:30 +0100

    Solve 2021-11 part 1

---
+++ 2021/src/2021/11.js
@@ -0,0 +1,104 @@
+const { linesOf } = require('../utils')
+
+const part1 = input => {
+  const cavern = Cavern.parseCavern(input)
+
+  let numberOfFlashes = 0
+  for (let step = 0; step < 100; step++) {
+    numberOfFlashes += cavern.nextStep()
+  }
+  return numberOfFlashes
+}
+
+class Octopus {
+  constructor (x, y, energy) {
+    this.x = x
+    this.y = y
+    this.energy = energy
+  }
+
+  reset () {
+    this.energy = 0
+    this.hasFlashed = false
+  }
+
+  flash () {
+    this.hasFlashed = true
+  }
+
+  get shouldFlash () {
+    return this.energy > 9 && !this.hasFlashed
+  }
+
+  increaseEnergy () {
+    this.energy++
+  }
+}
+
+class Cavern {
+  constructor (grid) {
+    this.grid = grid
+  }
+
+  nextStep () {
+    // 1. increase energy level of each octopus
+    this.octopuses.forEach(octopus => octopus.increaseEnergy())
+
+    // 2. trigger flash execution
+    const numberOfFlashesInStep = this.flash(this.octopuses)
+
+    // 3. reset all octopuses that flashed during round
+    this.octopuses.filter(octopus => octopus.hasFlashed).forEach(octopus => octopus.reset())
+
+    return numberOfFlashesInStep
+  }
+
+  flash (octopuses, numberOfPreviousFlashes = 0) {
+    const flashingOctopuses = octopuses.filter(octopus => octopus.shouldFlash)
+    if (octopuses.length === 0) {
+      return numberOfPreviousFlashes
+    }
+
+    // flash
+    flashingOctopuses.forEach(octopus => octopus.flash())
+
+    // increase energy level of all adjacent octopuses
+    const adjacentOctopuses = flashingOctopuses.flatMap(octopus => this.adjacentOctopusesFor(octopus))
+    adjacentOctopuses.forEach(octopus => octopus.increaseEnergy())
+
+    // trigger next flash execution
+    return this.flash(
+      [...new Set(adjacentOctopuses)],
+      numberOfPreviousFlashes + flashingOctopuses.length)
+  }
+
+  adjacentOctopusesFor ({ x, y }) {
+    return [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+      .map(([yd, xd]) => this.octopusAt(x + xd, y + yd))
+      .filter(octopus => !!octopus)
+  }
+
+  octopusAt (x, y) {
+    if (y < 0 || y === this.grid.length) {
+      return undefined
+    }
+    if (x < 0 || y === this.grid[y].length) {
+      return undefined
+    }
+    return this.grid[y][x]
+  }
+
+  get octopuses () {
+    return this.grid.flatMap(row => row)
+  }
+
+  static parseCavern (input) {
+    return new Cavern(
+      linesOf(input)
+        .map((row, y) =>
+          row.split('')
+            .map((cell, x) => new Octopus(x, y, Number(cell)))))
+  }
+}
+
+module.exports = { part1 }
---
+++ 2021/tests/2021/11.test.js
@@ -0,0 +1,34 @@
+const { readPuzzleInput } = require('../../src/utils')
+const { part1 } = require('../../src/2021/11')
+
+describe('2021 day 11', () => {
+  const example = `\
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+  describe('part 1', () => {
+    it('should solve example', () => {
+      const input = example
+
+      const result = part1(input)
+
+      expect(result).toBe(1656)
+    })
+
+    it('should solve my input', () => {
+      const input = readPuzzleInput(2021, 11)
+
+      const result = part1(input)
+
+      expect(result).toBe(1642)
+    })
+  })
+})
