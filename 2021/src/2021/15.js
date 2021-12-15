a2d2e6a1954e7833b4b745d87d213611b385e5fc
Michael Vitz <vitz.michael@googlemail.com>
2021-12-15 12:25:54 +0100

    Refactoring

--- 2021/src/2021/15.js
+++ 2021/src/2021/15.js
@@ -1,66 +1,20 @@
 const { linesOf } = require('../../src/utils')
 
 const part1 = input => {
-  const tile = parseTile(input)
-  const [start, end] = toMap(toNodes(tile))
-  aStar(start, end)
-  return end.risk
+  return solve(input, 1)
 }
 
 const part2 = input => {
-  const tile = parseTile(input)
-  const tiles = expandTile(tile, 5, 5)
+  return solve(input, 5)
+}
 
-  const [start, end] = toMap(toNodes(tiles))
+const solve = (input, expansion) => {
+  const tile = Tile.parse(input).expand(expansion, expansion)
+
+  const { start, end } = Map.fromTile(tile)
   aStar(start, end)
+
   return end.risk
-}
-
-const expandTile = (tile, vertical, horizontal) => {
-  // expand horizontal
-  const tileRow = []
-  for (const row of tile) {
-    const subRow = []
-    for (let i = 0; i < horizontal; i++) {
-      subRow.push(...row.map(risk => increaseRisk(risk, i)))
-    }
-    tileRow.push(subRow)
-  }
-
-  // expand vertically
-  const rows = []
-  for (let i = 0; i < vertical; i++) {
-    rows.push(...tileRow.map(row => row.map(risk => increaseRisk(risk, i))))
-  }
-
-  return rows
-}
-
-const toMap = rows => {
-  rows.forEach((row, y) => row.forEach((node, x) => {
-    // top neighbour
-    if (y > 0) {
-      node.append(rows[y - 1][x])
-    }
-    // right neighbour
-    if (x < row.length - 1) {
-      node.append(rows[y][x + 1])
-    }
-    // bottom neighbour
-    if (y < rows.length - 1) {
-      node.append(rows[y + 1][x])
-    }
-    // left neighbour
-    if (x > 0) {
-      node.append(rows[y][x - 1])
-    }
-  }))
-
-  const start = rows[0][0]
-  start.risk = 0
-
-  const end = rows[rows.length - 1][rows[0].length - 1]
-  return [start, end]
 }
 
 const aStar = (start, end) => {
@@ -99,6 +53,60 @@ const aStar = (start, end) => {
   throw new Error('No path found')
 }
 
+class Map {
+  constructor (nodes) {
+    this.nodes = nodes
+  }
+
+  get height () {
+    return this.nodes.length
+  }
+
+  get width () {
+    return this.nodes[0].length
+  }
+
+  get start () {
+    return this.nodes[0][0]
+  }
+
+  get end () {
+    return this.nodes[this.height - 1][this.width - 1]
+  }
+
+  static fromTile (tile) {
+    const nodes = Map.toNodes(tile.rows)
+
+    // link all nodes
+    nodes.forEach((row, y) => row.forEach((node, x) => {
+      // top neighbour
+      if (y > 0) {
+        node.append(nodes[y - 1][x])
+      }
+      // right neighbour
+      if (x < nodes.length - 1) {
+        node.append(nodes[y][x + 1])
+      }
+      // bottom neighbour
+      if (y < nodes.length - 1) {
+        node.append(nodes[y + 1][x])
+      }
+      // left neighbour
+      if (x > 0) {
+        node.append(nodes[y][x - 1])
+      }
+    }))
+
+    const map = new Map(nodes)
+    map.start.risk = 0
+    return map
+  }
+
+  static toNodes (rows) {
+    return rows.map(row => row.map(node => new Node(node)))
+  }
+}
+
 class Node {
   constructor (risk) {
     this.risk = risk
@@ -110,16 +118,45 @@ class Node {
   }
 }
 
-const toNodes = rows =>
-  rows.map(row => row.map(node => new Node(node)))
+class Tile {
+  constructor (rows) {
+    this.rows = rows
+  }
 
-const increaseRisk = (risk, increment) => {
-  const increasedRisk = risk + increment
-  return increasedRisk > 9 ? increasedRisk - 9 : increasedRisk
+  expand (vertical, horizontal) {
+    // expand horizontal
+    const tileRow = []
+    for (const row of this.rows) {
+      const subRow = []
+      for (let i = 0; i < horizontal; i++) {
+        subRow.push(...row.map(risk => Tile.increaseRisk(risk, i)))
+      }
+      tileRow.push(subRow)
+    }
+
+    // expand vertically
+    const rows = []
+    for (let i = 0; i < vertical; i++) {
+      rows.push(...tileRow.map(row => row.map(risk => Tile.increaseRisk(risk, i))))
+    }
+
+    return new Tile(rows)
+  }
+
+  toString () {
+    return this.rows.map(row => row.join('')).join('\n')
+  }
+
+  static parse (input) {
+    return new Tile(
+      linesOf(input)
+        .map(line => line.split('').map(Number)))
+  }
+
+  static increaseRisk (risk, increment) {
+    const increasedRisk = risk + increment
+    return increasedRisk > 9 ? increasedRisk - 9 : increasedRisk
+  }
 }
-
-const parseTile = input =>
-  linesOf(input)
-    .map(line => line.split('').map(Number))
 
 module.exports = { part1, part2 }
