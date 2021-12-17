0849c2c7be382e64a8c65c0ace22e980c998ae47
Michael Vitz <vitz.michael@googlemail.com>
2021-12-17 21:15:52 +0100

    Refactoring

--- 2021/src/2021/17.js
+++ 2021/src/2021/17.js
@@ -1,66 +1,67 @@
 const part1 = input => {
-  const [[tMinX, tMaxX], [tMaxY, tMinY]] = parseTarget(input)
+  const targetArea = parseTarget(input)
 
-  const xs = findPossibleXs(tMinX, tMaxX)
-  const initialVelocities = findInitialVelocities(xs, tMinX, tMaxX, tMinY, tMaxY)
+  const initialVelocities = findInitialVelocities(targetArea)
 
   return findMaxY(initialVelocities)
 }
 
 const part2 = input => {
-  const [[tMinX, tMaxX], [tMaxY, tMinY]] = parseTarget(input)
+  const targetArea = parseTarget(input)
 
-  const xs = findPossibleXs(tMinX, tMaxX)
-  const initialVelocities = findInitialVelocities(xs, tMinX, tMaxX, tMinY, tMaxY)
+  const initialVelocities = findInitialVelocities(targetArea)
 
   return initialVelocities.length
 }
 
 const findMaxY = initialVelocities => initialVelocities
   .map(([, y]) => y / 2 * (y + 1))
-  .sort((a, b) => a - b)
-  .reverse()[0]
+  .sort((a, b) => b - a)[0]
 
-const findInitialVelocities = (xs, tMinX, tMaxX, tMinY, tMaxY) => {
-  const initialVelocities = []
-  // TODO: find a way to calculate the upper bound
-  for (const x of xs) {
-    for (let y = tMaxY; y < 1_000; y++) {
-      if (hits(x, y, tMinX, tMaxX, tMinY, tMaxY)) {
-        initialVelocities.push([x, y])
-      }
-    }
-  }
-  return initialVelocities
-}
+const findInitialVelocities = (targetArea) =>
+  findPossibleXs(targetArea)
+    .flatMap(x =>
+      findPossibleYs(targetArea)
+        .filter(y => hits(x, y, targetArea))
+        .map(y => [x, y]))
 
-const hits = (cX, cY, tMinX, tMaxX, tMinY, tMaxY) => {
+const hits = (cX, cY, targetArea) => {
   let [x, y] = [0, 0]
-  while (x <= tMaxX && y >= tMaxY) {
+  while (x <= targetArea.right && y >= targetArea.bottom) {
     x += Math.max(0, cX--)
     y += cY--
 
-    if (x >= tMinX && x <= tMaxX && y <= tMinY && y >= tMaxY) {
+    if (targetArea.contains(x, y)) {
       return true
     }
   }
   return false
 }
 
-const findPossibleXs = (tMinX, tMaxX) => {
+const findPossibleXs = ({ left, right }) => {
   const possibleXs = []
-  for (let x = tMinX; x <= tMaxX; x++) {
+  for (let x = left; x <= right; x++) {
     possibleXs.push(x)
   }
-  for (let x = Math.round(tMaxX / 2); (x / 2 * (x + 1)) >= tMinX; x--) {
+  // max distance for X can be calculated via gauss summation and must be behind left border
+  for (let x = Math.round(right / 2); (x / 2 * (x + 1)) >= left; x--) {
     possibleXs.push(x)
   }
   return possibleXs
 }
 
+const findPossibleYs = ({ bottom }) => {
+  const ys = []
+  // TODO: find a way to calculate the upper bound
+  for (let y = bottom; y < 1_000; y++) {
+    ys.push(y)
+  }
+  return ys
+}
+
 const parseTarget = input => {
   const [, coordinates] = input.split(': ')
-  return coordinates.split(', ').map(parseRange)
+  return new Area(coordinates.split(', ').map(parseRange))
 }
 
 const parseRange = input => input
@@ -68,5 +69,33 @@ const parseRange = input => input
   .split('..')
   .map(Number)
   .sort((a, b) => a - b)
+
+class Area {
+  constructor ([x, y]) {
+    this.x = x
+    this.y = y
+  }
+
+  contains (x, y) {
+    return x >= this.left && x <= this.right &&
+      y >= this.bottom && y <= this.top
+  }
+
+  get left () {
+    return this.x[0]
+  }
+
+  get right () {
+    return this.x[1]
+  }
+
+  get bottom () {
+    return this.y[0]
+  }
+
+  get top () {
+    return this.y[1]
+  }
+}
 
 module.exports = { part1, part2 }
