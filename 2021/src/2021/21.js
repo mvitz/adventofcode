11bcf13c5ebd4da33783fc4cc86d903e68974869
Michael Vitz <vitz.michael@googlemail.com>
2021-12-22 12:29:11 +0100

    Skip 2021-21 part 2

--- 2021/src/2021/21.js
+++ 2021/src/2021/21.js
@@ -6,43 +6,88 @@ const part1 = input => {
   const board = new Board(startingPositions)
   const dice = new DeterministicDice()
 
-  const game = new Game(board, dice)
+  const game = new Game(board, 1000)
 
   while (!game.hasWinner) {
-    game.makeTurn()
+    const steps = dice.roll() + dice.roll() + dice.roll()
+    game.makeTurn(steps)
   }
 
   return game.losingScore * dice.rolls
+}
+
+const part2 = input => {
+  const startingPositions = parse(input)
+
+  const board = new Board(startingPositions)
+
+  const wins = [0, 0]
+
+  let games = [[new Game(board, 21), 1]]
+  while (games.length > 0) {
+    const ongoingGames = []
+    for (const [game, combinations] of games) {
+      if (game.hasWinner) {
+        wins[game.winner] += combinations
+      } else {
+        for (const [steps, possibilities] of [
+          [3, 1],
+          [4, 3],
+          [5, 6],
+          [6, 7],
+          [7, 6],
+          [8, 3],
+          [9, 1]
+        ]) {
+          ongoingGames.push([game.copy().makeTurn(steps), combinations * possibilities])
+        }
+      }
+    }
+    games = ongoingGames
+  }
+
+  return wins
 }
 
 const parse = input =>
   linesOf(input).map(line => line.substring(line.length - 1)).map(Number)
 
 class Game {
-  constructor (board, dice) {
+  constructor (board, winningScore) {
     this.board = board
-    this.dice = dice
+    this.winningScore = winningScore
     this.score = [0, 0]
     this.currentPlayer = 0
   }
 
   get hasWinner () {
-    return this.score.some(score => score >= 1000)
+    return this.score.some(score => score >= this.winningScore)
   }
 
-  makeTurn () {
-    const { board, dice, score, currentPlayer } = this
+  makeTurn (steps) {
+    const { board, score, currentPlayer } = this
 
-    const steps = dice.roll() + dice.roll() + dice.roll()
     const playerPosition = board.movePawn(currentPlayer, steps)
-
     score[currentPlayer] += playerPosition
 
     this.currentPlayer = (currentPlayer + 1) % 2
+
+    return this
   }
 
   get losingScore () {
     return Math.min(...this.score)
+  }
+
+  get winner () {
+    return this.score[0] >= this.winningScore ? 0 : 1
+  }
+
+  copy () {
+    const game = new Game(this.board.copy(), this.winningScore)
+    game.score = [...this.score]
+    game.currentPlayer = this.currentPlayer
+    return game
   }
 }
 
@@ -56,6 +101,10 @@ class Board {
     const newPosition = ((position + steps - 1) % 10) + 1
     this.pawnPositions[player] = newPosition
     return newPosition
+  }
+
+  copy () {
+    return new Board([...this.pawnPositions])
   }
 }
 
@@ -74,4 +123,4 @@ class DeterministicDice {
   }
 }
 
-module.exports = { part1 }
+module.exports = { part1, part2 }
--- 2021/tests/2021/21.test.js
+++ 2021/tests/2021/21.test.js
@@ -1,5 +1,5 @@
 const { readPuzzleInput } = require('../../src/utils')
-const { part1 } = require('../../src/2021/21')
+const { part1, part2 } = require('../../src/2021/21')
 
 describe('2021 day 21', () => {
   const example = `\
@@ -21,6 +21,24 @@ Player 2 starting position: 8`
       const result = part1(input)
 
       expect(result).toBe(598416)
+    })
+  })
+
+  describe.skip('part 2', () => {
+    it('should solve example', () => {
+      const input = example
+
+      const result = part2(input)
+
+      expect(result).toBe(444356092776315)
+    })
+
+    it.skip('should solve my input', () => {
+      const input = readPuzzleInput(2021, 21)
+
+      const result = part2(input)
+
+      expect(result).toBe(-2)
     })
   })
 })
