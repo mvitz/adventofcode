b17dadddde4428e64ec28e62d5202802c205aa9f
Michael Vitz <vitz.michael@googlemail.com>
2021-12-27 13:33:47 +0100

    Skip 2021-22 part 2

--- 2021/src/2021/22.js
+++ 2021/src/2021/22.js
@@ -1,38 +1,46 @@
 const { linesOf } = require('../utils')
 
 const part1 = input => {
+  return solve(input, point => point >= -50 && point <= 50)
+}
+
+const part2 = input => {
+  return solve(input, _ => true)
+}
+
+const solve = (input, filter) => {
   const reactorCore = new ReactorCore()
 
-  parseInput(input).forEach(([action, cubes]) =>
+  parseInput(input, filter).forEach(([action, cubes]) =>
     cubes.forEach(cube => reactorCore.turn(action, cube)))
 
   return reactorCore.onCubes.size
 }
 
-const parseInput = input => {
+const parseInput = (input, filter) => {
   return linesOf(input).map(line => {
     const [action, cuboid] = line.split(' ')
-    const cubes = parseCuboid(cuboid)
+    const cubes = parseCuboid(cuboid, filter)
     return [action, cubes]
   })
 }
 
-const parseCuboid = cuboid => {
-  const [xr, yr, zr] = cuboid.split(',').map(parseCoordinate)
+const parseCuboid = (cuboid, filter) => {
+  const [xr, yr, zr] = cuboid.split(',')
+    .map(coordinate => parseCoordinate(coordinate, filter))
 
   const cubes = []
   xr.forEach(x => yr.forEach(y => zr.forEach(z => cubes.push([x, y, z]))))
   return cubes
 }
 
-const parseCoordinate = input => {
+const parseCoordinate = (input, filter) => {
   const [start, end] = input.substring(2).split('..').map(Number)
   const result = []
   for (let i = start; i <= end; i++) {
     result.push(i)
   }
-  return result
-    .filter(point => point >= -50 && point <= 50)
+  return result.filter(filter)
 }
 
 class ReactorCore {
@@ -64,4 +72,4 @@ class ReactorCore {
   }
 }
 
-module.exports = { part1 }
+module.exports = { part1, part2 }
