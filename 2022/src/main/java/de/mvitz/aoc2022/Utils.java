1be91e07c641c4232488999c8072d432d5f4beb9
Michael Vitz <vitz.michael@googlemail.com>
2022-12-01 13:29:39 +0100

    2022: Day  1 - Refactoring

--- 2022/src/main/java/de/mvitz/aoc2022/Utils.java
+++ 2022/src/main/java/de/mvitz/aoc2022/Utils.java
@@ -1,18 +1,23 @@
 package de.mvitz.aoc2022;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static java.util.Objects.requireNonNull;
 
 final class Utils {
 
     private Utils() {
     }
 
+    @SuppressWarnings("java:S112")
     public static String contentOf(String name) {
         try {
-            final var resource = Utils.class.getResource("/" + name).toURI();
+            final var resource = requireNonNull(Utils.class.getResource("/" + name)).toURI();
             return Files.readString(Path.of(resource));
-        } catch (Exception e) {
+        } catch (URISyntaxException | IOException e) {
             throw new RuntimeException("Unable to read content of: " + name, e);
         }
     }
