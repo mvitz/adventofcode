88a75d7ec793b48e9a77f2a58ceabb58e9ffc87d
Michael Vitz <vitz.michael@googlemail.com>
2022-12-05 09:34:13 +0100

    2022: Day  5 - Refactoring

--- 2022/src/main/java/de/mvitz/aoc2022/Day05.java
+++ 2022/src/main/java/de/mvitz/aoc2022/Day05.java
@@ -1,17 +1,19 @@
 package de.mvitz.aoc2022;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
-import java.util.Stack;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.joining;
 
 final class Day05 {
 
     private Day05() {
     }
+
+    private static final Pattern REARRANGEMENT_PROCEDURE_PATTERN =
+            Pattern.compile("move (\\d+) from (\\d+) to (\\d+)");
 
     public static String findCrateOnTopMessageFor(int crateMoverVersion, String input) {
         final var startingStackAndRearrangementProcedure = input.split("\n\n");
@@ -20,42 +22,46 @@ final class Day05 {
 
         startingStackAndRearrangementProcedure[1]
                 .lines()
-                .forEach(line -> {
-                    final var m = Pattern.compile("move (\\d+) from (\\d+) to (\\d+)").matcher(line);
-                    if (m.matches()) {
-                        final var number = Integer.parseInt(m.group(1));
-                        final var from = Integer.parseInt(m.group(2));
-                        final var to = Integer.parseInt(m.group(3));
-                        stacks.move(number, from, to);
-                    }
-                });
+                .forEach(rearrangementProcedure -> execute(stacks, rearrangementProcedure));
 
-        return stacks.topCrates().stream().collect(joining(""));
+        return String.join("", stacks.topCrates());
+    }
+
+    private static void execute(Stacks stacks, String line) {
+        final var matcher = REARRANGEMENT_PROCEDURE_PATTERN.matcher(line);
+        if (matcher.matches()) {
+            final var number = Integer.parseInt(matcher.group(1));
+            final var from = Integer.parseInt(matcher.group(2));
+            final var to = Integer.parseInt(matcher.group(3));
+            stacks.move(number, from, to);
+        }
     }
 
     private static final class Stacks {
 
         private final int crateMoverVersion;
-        private final Stack<String>[] stacks;
+        private final Deque<String>[] stacks;
 
+        @SuppressWarnings("unchecked")
         private Stacks(int crateMoverVersion, int numberOfStacks) {
             this.crateMoverVersion = crateMoverVersion;
-            this.stacks = new Stack[numberOfStacks];
-            IntStream.range(0, numberOfStacks).forEach(number -> stacks[number] = new Stack<>());
+            this.stacks = new Deque[numberOfStacks];
+            IntStream.range(0, numberOfStacks)
+                    .forEach(number -> stacks[number] = new ArrayDeque<>());
         }
 
         public List<String> topCrates() {
             return Arrays.stream(stacks)
-                    .map(Stack::peek)
+                    .map(Deque::peekLast)
                     .toList();
         }
 
         public void push(int stack, String crate) {
-            stacks[stack - 1].push(crate);
+            stacks[stack - 1].addLast(crate);
         }
 
         private String pop(int stack) {
-            return stacks[stack - 1].pop();
+            return stacks[stack - 1].pollLast();
         }
 
         public void move(int number, int from, int to) {
@@ -65,13 +71,11 @@ final class Day05 {
                     push(to, crate);
                 }
             } else if (crateMoverVersion == 9001) {
-                final var tmp = new Stack<String>();
+                final var tmp = new ArrayDeque<String>();
                 for (int i = 0; i < number; i++) {
-                    tmp.add(pop(from));
+                    tmp.addFirst(pop(from));
                 }
-                for (int i = 0; i < number; i++) {
-                    push(to, tmp.pop());
-                }
+                tmp.forEach(crate -> push(to, crate));
             }
         }
 
