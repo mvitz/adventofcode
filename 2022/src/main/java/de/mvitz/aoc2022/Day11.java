9004001c4b59ea44142e77140238e4e011fa9c64
Michael Vitz <vitz.michael@googlemail.com>
2022-12-11 19:13:23 +0100

    2022: Day 11 - Refactoring

--- 2022/src/main/java/de/mvitz/aoc2022/Day11.java
+++ 2022/src/main/java/de/mvitz/aoc2022/Day11.java
@@ -1,76 +1,85 @@
 package de.mvitz.aoc2022;
 
+import java.math.BigInteger;
 import java.util.*;
-import java.util.function.IntConsumer;
-import java.util.function.IntUnaryOperator;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+import static java.util.Comparator.comparing;
 
 final class Day11 {
 
     private Day11() {
     }
 
-    public static int findLevelOfMonkeyBusinessAfter20RoundsFor(String input) {
-        final var monkeys = parse(input);
+    public static long findLevelOfMonkeyBusinessAfterRoundsFor(
+            String input,
+            UnaryOperator<BigInteger> worryLevelReduction,
+            int rounds) {
+        final var monkeys = parse(input, worryLevelReduction);
 
-        for (int i = 0; i < 20; i++) {
+        for (var i = 0; i < rounds; i++) {
             monkeys.forEach(Monkey::takeTurn);
         }
 
         return monkeys.stream()
-                .sorted(Comparator.comparing(Monkey::numberOfExpectedItems).reversed())
-                .mapToInt(Monkey::numberOfExpectedItems)
+                .sorted(comparing(Monkey::numberOfExpectedItems).reversed())
+                .mapToLong(Monkey::numberOfExpectedItems)
                 .limit(2)
                 .reduce((first, second) -> first * second)
                 .orElse(-1);
     }
 
-    private static List<Monkey> parse(String input) {
+    private static List<Monkey> parse(String input,
+                                      UnaryOperator<BigInteger> worryLevelReduction) {
         final var monkeys = new ArrayList<Monkey>();
 
         Arrays.stream(input.split("\n\n"))
-                .map(monkey -> createMonkey(monkey, monkeys))
+                .map(monkey -> createMonkey(monkey, worryLevelReduction, monkeys))
                 .forEach(monkeys::add);
 
         return monkeys;
     }
 
-    private static Monkey createMonkey(String input, List<Monkey> otherMonkeys) {
+    private static Monkey createMonkey(String input,
+                                       UnaryOperator<BigInteger> worryLevelReduction,
+                                       List<Monkey> otherMonkeys) {
         final var lines = input.lines().toList();
 
         final var operation = parseOperation(lines.get(2));
         final var throwingAction = parseThrowingAction(lines.get(3), lines.get(4), lines.get(5), otherMonkeys);
 
-        final var monkey = new Monkey(operation, throwingAction);
+        final var monkey = new Monkey(operation, worryLevelReduction, throwingAction);
 
         Arrays.stream(lines.get(1).substring("  Starting items: ".length()).split(", "))
-                .mapToInt(Integer::parseInt)
+                .map(BigInteger::new)
                 .forEach(monkey::addItem);
 
         return monkey;
     }
 
-    private static IntUnaryOperator parseOperation(String line) {
+    private static UnaryOperator<BigInteger> parseOperation(String line) {
         final String operation = line.substring("  Operation: new = ".length());
         if ("old * old".equals(operation)) {
-            return old -> old * old;
+            return old -> old.multiply(old);
         } else if (operation.startsWith("old * ")) {
-            final var value = Integer.parseInt(operation.substring("old * ".length()));
-            return old -> old * value;
+            final var value = new BigInteger(operation.substring("old * ".length()));
+            return old -> old.multiply(value);
         } else if (operation.startsWith("old + ")) {
-            final var value = Integer.parseInt(operation.substring("old + ".length()));
-            return old -> old + value;
+            final var value = new BigInteger(operation.substring("old + ".length()));
+            return old -> old.add(value);
         } else {
             throw new IllegalArgumentException("Unknown operation: " + line);
         }
     }
 
-    private static IntConsumer parseThrowingAction(String test,
-                                                   String ifTrue, String ifFalse,
-                                                   List<Monkey> monkeys) {
+    private static Consumer<BigInteger> parseThrowingAction(String test,
+                                                            String ifTrue, String ifFalse,
+                                                            List<Monkey> monkeys) {
         if (!test.startsWith("  Test: divisible by ")) {
             throw new IllegalArgumentException("Unknown test: " + test);
         }
-        final var testValue = Integer.parseInt(test.substring("  Test: divisible by ".length()));
+        final var testValue = new BigInteger(test.substring("  Test: divisible by ".length()));
 
         if (!ifTrue.startsWith("    If true: throw to monkey ")) {
             throw new IllegalArgumentException("Unknown ifTrue: " + ifTrue);
@@ -83,7 +92,7 @@ final class Day11 {
         final var ifFalseThrowTo = Integer.parseInt(ifFalse.substring("    If false: throw to monkey ".length()));
 
         return worryLevel -> {
-            if (worryLevel % testValue == 0) {
+            if (BigInteger.ZERO.equals(worryLevel.remainder(testValue))) {
                 monkeys.get(ifTrueThrowTo).addItem(worryLevel);
             } else {
                 monkeys.get(ifFalseThrowTo).addItem(worryLevel);
@@ -93,19 +102,23 @@ final class Day11 {
 
     private static final class Monkey {
 
-        private final Deque<Integer> items = new ArrayDeque<>();
+        private final Deque<BigInteger> items = new ArrayDeque<>();
 
-        private final IntUnaryOperator operation;
-        private final IntConsumer throwingAction;
+        private final UnaryOperator<BigInteger> operation;
+        private final UnaryOperator<BigInteger> worryLevelReduction;
+        private final Consumer<BigInteger> throwingAction;
 
-        private int numberOfExpectedItems = 0;
+        private long numberOfExpectedItems = 0;
 
-        private Monkey(IntUnaryOperator operation, IntConsumer throwingAction) {
+        private Monkey(UnaryOperator<BigInteger> operation,
+                       UnaryOperator<BigInteger> worryLevelReduction,
+                       Consumer<BigInteger> throwingAction) {
             this.operation = operation;
+            this.worryLevelReduction = worryLevelReduction;
             this.throwingAction = throwingAction;
         }
 
-        public void addItem(int worryLevel) {
+        public void addItem(BigInteger worryLevel) {
             items.addLast(worryLevel);
         }
 
@@ -122,8 +135,8 @@ final class Day11 {
             }
 
             var worryLevel = items.pollFirst();
-            worryLevel = operation.applyAsInt(worryLevel);
-            worryLevel /= 3;
+            worryLevel = operation.apply(worryLevel);
+            worryLevel = worryLevelReduction.apply(worryLevel);
 
             throwingAction.accept(worryLevel);
 
@@ -132,7 +145,7 @@ final class Day11 {
             return false;
         }
 
-        public int numberOfExpectedItems() {
+        public long numberOfExpectedItems() {
             return numberOfExpectedItems;
         }
     }
--- 2022/src/test/java/de/mvitz/aoc2022/Day11Tests.java
+++ 2022/src/test/java/de/mvitz/aoc2022/Day11Tests.java
@@ -1,8 +1,12 @@
 package de.mvitz.aoc2022;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static de.mvitz.aoc2022.Day11.findLevelOfMonkeyBusinessAfter20RoundsFor;
+import java.math.BigInteger;
+import java.util.function.UnaryOperator;
+
+import static de.mvitz.aoc2022.Day11.findLevelOfMonkeyBusinessAfterRoundsFor;
 import static de.mvitz.aoc2022.Utils.contentOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -13,17 +17,27 @@ class Day11Tests {
     void part1_example() {
         var input = contentOf("day11_example.txt");
 
-        var levelOfMonkeyBusinessAfter20Rounds = findLevelOfMonkeyBusinessAfter20RoundsFor(input);
+        var levelOfMonkeyBusinessAfter20Rounds = findLevelOfMonkeyBusinessAfterRoundsFor(input, worryLevel -> worryLevel.divide(new BigInteger("3")), 20);
 
-        assertThat(levelOfMonkeyBusinessAfter20Rounds, is(10_605));
+        assertThat(levelOfMonkeyBusinessAfter20Rounds, is(10_605L));
     }
 
     @Test
     void part1() {
         var input = contentOf("day11.txt");
 
-        var levelOfMonkeyBusinessAfter20Rounds = findLevelOfMonkeyBusinessAfter20RoundsFor(input);
+        var levelOfMonkeyBusinessAfter20Rounds = findLevelOfMonkeyBusinessAfterRoundsFor(input, worryLevel -> worryLevel.divide(new BigInteger("3")), 20);
 
-        assertThat(levelOfMonkeyBusinessAfter20Rounds, is(57_838));
+        assertThat(levelOfMonkeyBusinessAfter20Rounds, is(57_838L));
+    }
+
+    @Test
+    @Disabled
+    void part2_example() {
+        var input = contentOf("day11_example.txt");
+
+        var levelOfMonkeyBusinessAfter20Rounds = findLevelOfMonkeyBusinessAfterRoundsFor(input, UnaryOperator.identity(), 10_000);
+
+        assertThat(levelOfMonkeyBusinessAfter20Rounds, is(2_713_310_158L));
     }
 }
