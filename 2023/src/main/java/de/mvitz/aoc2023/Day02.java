a4e5664d5dbae9643dc23a1906a59031f6a7dee2
Michael Vitz <vitz.michael@googlemail.com>
2023-12-02 12:13:25 +0100

    2023: Day  2 - Part 2

--- 2023/src/main/java/de/mvitz/aoc2023/Day02.java
+++ 2023/src/main/java/de/mvitz/aoc2023/Day02.java
@@ -1,6 +1,10 @@
 package de.mvitz.aoc2023;
 
+import de.mvitz.aoc2023.Day02.Game.Cubes;
+import de.mvitz.aoc2023.Day02.Game.Rounds.Round;
+
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -17,12 +21,26 @@ final class Day02 {
 				.sum();
 	}
 
+	static long sumOfMinimumCubesPower(String input) {
+		return input.lines()
+				.map(Game::from)
+				.map(Game::minimalCubes)
+				.mapToLong(Cubes::power)
+				.sum();
+	}
+
 	record Game(long id, Cubes cubes, Rounds rounds) {
 
 		boolean isPossible() {
-			return rounds.everyContainsLessThan(cubes);
+			return rounds.eachContainsAtLeast(cubes);
 		}
 
+		Cubes minimalCubes() {
+			return rounds.each()
+					.map(Round::cubes)
+					.reduce(Cubes::union)
+					.orElseThrow();
+		}
 
 		static Game from(String input) {
 			return new Game(
@@ -31,33 +49,48 @@ final class Day02 {
 					Rounds.from(input.substring(input.indexOf(':') + 2)));
 		}
 
-		record Cubes(List<Cube> value) {
+		record Cubes(EnumMap<Color, Long> value) {
 
 			boolean isSubsetOf(Cubes cubes) {
-				return Arrays.stream(Color.values())
+				return Color.each()
 						.allMatch(color -> countOfColored(color) <= cubes.countOfColored(color));
 			}
 
 			long countOfColored(Color color) {
-				return value.stream()
-						.filter(cube -> cube.isColored(color))
-						.count();
+				return value.getOrDefault(color, 0L);
+			}
+
+			Cubes union(Cubes cubes) {
+				var newValue = new EnumMap<Color, Long>(Color.class);
+				Color.each().forEach(color ->
+						newValue.put(color, Math.max(countOfColored(color), cubes.countOfColored(color))));
+				return new Cubes(newValue);
+			}
+
+			long power() {
+				return value.values().stream()
+						.reduce((a, b) -> a * b)
+						.orElse(0L);
 			}
 
 			static Cubes from(String input) {
-				return new Cubes(
-						Arrays.stream(input.strip().split(", "))
-								.map(coloredCubes -> coloredCubes.split(" "))
-								.flatMap(numberColor -> Stream.generate(() -> Cube.from(numberColor[1])).limit(Long.parseLong(numberColor[0])))
-								.toList());
+				var cubes = new EnumMap<Color, Long>(Color.class);
+				Arrays.stream(input.strip().split(", "))
+						.map(coloredCubes -> coloredCubes.split(" "))
+						.forEach(numberColor -> cubes.put(Color.from(numberColor[1]), Long.parseLong(numberColor[0])));
+				return new Cubes(cubes);
 			}
 		}
 
 		record Rounds(List<Round> value) {
 
-			boolean everyContainsLessThan(Cubes cubes) {
+			boolean eachContainsAtLeast(Cubes cubes) {
 				return value.stream()
 						.allMatch(round -> round.containsLessThan(cubes));
+			}
+
+			Stream<Round> each() {
+				return value.stream();
 			}
 
 			static Rounds from(String input) {
@@ -79,19 +112,12 @@ final class Day02 {
 			}
 		}
 
-		record Cube(Color color) {
-
-			boolean isColored(Color color) {
-				return this.color == color;
-			}
-
-			static Cube from(String input) {
-				return new Cube(Color.from(input));
-			}
-		}
-
 		enum Color {
 			BLUE, RED, GREEN;
+
+			static Stream<Color> each() {
+				return Arrays.stream(values());
+			}
 
 			static Color from(String input) {
 				return valueOf(input.strip().toUpperCase());
--- 2023/src/test/java/de/mvitz/aoc2023/Day02Tests.java
+++ 2023/src/test/java/de/mvitz/aoc2023/Day02Tests.java
@@ -2,6 +2,7 @@ package de.mvitz.aoc2023;
 
 import org.junit.jupiter.api.Test;
 
+import static de.mvitz.aoc2023.Day02.sumOfMinimumCubesPower;
 import static de.mvitz.aoc2023.Day02.sumOfPossibleGameIDs;
 import static de.mvitz.aoc2023.Utils.contentOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -25,5 +26,23 @@ class Day02Tests {
 		var solution = sumOfPossibleGameIDs(input);
 
 		assertThat(solution, is(1_867L));
+	}
+
+	@Test
+	void part2_example() {
+		var input = contentOf("day02_example.txt");
+
+		var solution = sumOfMinimumCubesPower(input);
+
+		assertThat(solution, is(2_286L));
+	}
+
+	@Test
+	void part2() {
+		var input = contentOf("day02.txt");
+
+		var solution = sumOfMinimumCubesPower(input);
+
+		assertThat(solution, is(84_538L));
 	}
 }
