e522cd5ceaf512f3cf0c380c6062736598bf3901
Michael Vitz <vitz.michael@googlemail.com>
2023-12-05 09:07:17 +0100

    2023: Day  5 - Part 2
    Code is correct but only example works because real data is too large.

--- 2023/src/main/java/de/mvitz/aoc2023/Day05.java
+++ 2023/src/main/java/de/mvitz/aoc2023/Day05.java
@@ -1,65 +1,150 @@
 package de.mvitz.aoc2023;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-
-import static java.util.Comparator.naturalOrder;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 final class Day05 {
 
 	private Day05() {
 	}
 
-	static long lowestLocationNumber(String input) {
-		var it = input.lines().iterator();
-
-		var seeds = new ArrayList<>(parseSeeds(it.next()));
-		it.next(); // skip empty line
-
-		var locations = findLocationsFor(seeds, it);
-
-		return locations.stream().min(naturalOrder()).orElseThrow();
+	static long lowestLocationNumberValues(String input) {
+		return Almanac.from(input, Almanac.Seeds::valuesFrom)
+				.findLowestLocationNumber();
 	}
 
-	private static List<Long> parseSeeds(String line) {
-		return Arrays.stream(line.substring("seeds: ".length()).split(" "))
-				.map(String::strip)
-				.map(Long::parseLong)
-				.toList();
+	static long lowestLocationNumberRanges(String input) {
+		return Almanac.from(input, Almanac.Seeds::rangesFrom)
+				.findLowestLocationNumber();
 	}
 
-	private static List<Long> findLocationsFor(List<Long> input, Iterator<String> lines) {
-		var out = new ArrayList<Long>();
+	record Range(long lower, long upper) {
 
-		lines.next(); // skip heading
-		while (lines.hasNext()) {
-			var line = lines.next();
-			if (line.isBlank()) {
-				break;
+		public boolean contains(long number) {
+			return number >= lower && number <= upper;
+		}
+
+		static Range from(long start, long length) {
+			return new Range(start, start + length - 1);
+		}
+
+		static Range of(long number) {
+			return new Range(number, number);
+		}
+	}
+
+	record Almanac(Seeds seeds, List<TransformationMap> maps) {
+
+		public long findLowestLocationNumber() {
+			return LongStream.iterate(0, location -> location + 1)
+					.parallel()
+					.filter(this::isValidLocation)
+					.findFirst()
+					.orElseThrow();
+		}
+
+		private boolean isValidLocation(long number) {
+			for (var map : maps) {
+				number = map.convert(number);
+			}
+			return seeds.contains(number);
+		}
+
+		public static Almanac from(String input, Function<Iterator<String>, Seeds> seedsParser) {
+			var lines = input.lines().iterator();
+
+			var seeds = seedsParser.apply(lines);
+
+			var maps = new ArrayList<TransformationMap>();
+			while (lines.hasNext()) {
+				maps.add(TransformationMap.from(lines));
 			}
 
-			var entry = line.split(" ");
-			var destination = Long.parseLong(entry[0]);
-			var source = Long.parseLong(entry[1]);
-			var range = Long.parseLong(entry[2]);
+			return new Almanac(seeds, maps.reversed());
+		}
 
-			var it = input.iterator();
-			while (it.hasNext()) {
-				var number = it.next();
-				if (number >= source && number < (source + range)) {
-					it.remove();
-					out.add(destination + number - source);
+		static final class Seeds {
+
+			private final List<Range> values;
+
+			private Seeds(List<Range> values) {
+				this.values = values;
+			}
+
+			public boolean contains(long number) {
+				for (var value : values) {
+					if (value.contains(number)) {
+						return true;
+					}
 				}
+				return false;
+			}
+
+			public static Seeds valuesFrom(Iterator<String> lines) {
+				var line = lines.next();
+				lines.next(); // skip empty line
+
+				return new Seeds(Arrays.stream(line.substring("seeds: ".length()).split(" "))
+						.map(String::strip)
+						.map(Long::parseLong)
+						.map(Range::of)
+						.toList());
+			}
+
+			public static Seeds rangesFrom(Iterator<String> lines) {
+				var values = valuesFrom(lines).values;
+
+				return new Seeds(Stream.iterate(0, i -> i < values.size(), i -> i + 2)
+						.map(i -> Range.from(values.get(i).lower, values.get(i + 1).lower))
+						.toList());
 			}
 		}
 
-		out.addAll(input);
+		static final class TransformationMap {
 
-		if (lines.hasNext()) {
-			return findLocationsFor(out, lines);
+			private final Map<Range, Long> entries = new HashMap<>();
+
+			private TransformationMap() {
+			}
+
+			private void put(Range key, long change) {
+				entries.put(key, change);
+			}
+
+			public long convert(long number) {
+				return entries.entrySet().stream()
+						.filter(entry -> entry.getKey().contains(number))
+						.map(Map.Entry::getValue)
+						.mapToLong(value -> number + value)
+						.findFirst()
+						.orElse(number);
+			}
+
+			public static TransformationMap from(Iterator<String> lines) {
+				var map = new TransformationMap();
+
+				lines.next(); // skip heading
+
+				while (lines.hasNext()) {
+					var line = lines.next();
+
+					if (line.isBlank()) {
+						return map;
+					}
+
+					var entry = line.split(" ");
+
+					var destination = Long.parseLong(entry[0]);
+					var source = Long.parseLong(entry[1]);
+					var range = Long.parseLong(entry[2]);
+
+					map.put(Range.from(destination, range), source - destination);
+				}
+
+				return map;
+			}
 		}
-		return out;
 	}
 }
--- 2023/src/test/java/de/mvitz/aoc2023/Day05Tests.java
+++ 2023/src/test/java/de/mvitz/aoc2023/Day05Tests.java
@@ -1,29 +1,55 @@
 package de.mvitz.aoc2023;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static de.mvitz.aoc2023.Day05.lowestLocationNumber;
+import static de.mvitz.aoc2023.Day05.lowestLocationNumberValues;
+import static de.mvitz.aoc2023.Day05.lowestLocationNumberRanges;
 import static de.mvitz.aoc2023.Utils.contentOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Day05Tests {
 
 	@Test
+	@Order(0)
 	void part1_example() {
 		var input = contentOf("day05_example.txt");
 
-		var solution = lowestLocationNumber(input);
+		var solution = lowestLocationNumberValues(input);
 
 		assertThat(solution, is(35L));
 	}
 
+	@Disabled("Takes to long to run regularly")
 	@Test
+	@Order(1)
 	void part1() {
 		var input = contentOf("day05.txt");
 
-		var solution = lowestLocationNumber(input);
+		var solution = lowestLocationNumberValues(input);
 
 		assertThat(solution, is(26_273_516L));
+	}
+
+	@Test
+	@Order(2)
+	void part2_example() {
+		var input = contentOf("day05_example.txt");
+
+		var solution = lowestLocationNumberRanges(input);
+
+		assertThat(solution, is(46L));
+	}
+
+	@Disabled("Takes to long to run regularly")
+	@Test
+	@Order(3)
+	void part2() {
+		var input = contentOf("day05.txt");
+
+		var solution = lowestLocationNumberRanges(input);
+
+		assertThat(solution, is(34_039_469L));
 	}
 }
