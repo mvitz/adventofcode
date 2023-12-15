8ebe0c3d0cddd0fa9f9212bf5c0ddfcac6011b9f
Michael Vitz <vitz.michael@googlemail.com>
2023-12-15 12:16:43 +0100

    2023: Day 11 - Part 2

--- 2023/src/main/java/de/mvitz/aoc2023/Day11.java
+++ 2023/src/main/java/de/mvitz/aoc2023/Day11.java
@@ -11,14 +11,18 @@ final class Day11 {
 	}
 
 	static long sumOfAllShortestPathsBetweenGalaxyPairs(String input) {
+		return sumOfAllShortestPathsBetweenOlderGalaxyPairs(input, 2);
+	}
+
+	static long sumOfAllShortestPathsBetweenOlderGalaxyPairs(String input, long factor) {
 		return Universe.parse(input)
-				.expand()
+				.expand(factor - 1)
 				.galaxyPairs().stream()
 				.mapToLong(Pair::shortestPathBetween)
 				.sum();
 	}
 
-	record Point(int x, int y) {
+	record Point(long x, long y) {
 	}
 
 	record Pair(Point first, Point second) {
@@ -30,8 +34,8 @@ final class Day11 {
 
 	record Universe(List<Point> galaxies) {
 
-		Universe expand() {
-			return new Universe(shiftColumns(shiftRows(galaxies)));
+		Universe expand(long factor) {
+			return new Universe(shiftColumns(shiftRows(galaxies, factor), factor));
 		}
 
 		Set<Pair> galaxyPairs() {
@@ -63,14 +67,14 @@ final class Day11 {
 			return new Universe(galaxies);
 		}
 
-		private static List<Point> shiftRows(List<Point> galaxies) {
+		private static List<Point> shiftRows(List<Point> galaxies, long factor) {
 			var shiftedGalaxies = new ArrayList<Point>();
 
-			var shift = 0;
-			for (int row = 0; row < rows(galaxies); row++) {
+			var shift = 0L;
+			for (var row = 0; row < rows(galaxies); row++) {
 				var galaxiesInRow = galaxiesInRow(galaxies, row);
 				if (galaxiesInRow.isEmpty()) {
-					shift++;
+					shift += factor;
 				} else {
 					for (var galaxy : galaxiesInRow) {
 						shiftedGalaxies.add(new Point(galaxy.x, galaxy.y + shift));
@@ -81,27 +85,27 @@ final class Day11 {
 			return shiftedGalaxies;
 		}
 
-		private static int rows(List<Point> galaxies) {
+		private static long rows(List<Point> galaxies) {
 			return galaxies.stream()
-						   .mapToInt(Point::y)
+						   .mapToLong(Point::y)
 						   .max()
 						   .orElse(-1) + 1;
 		}
 
-		private static List<Point> galaxiesInRow(List<Point> galaxies, int row) {
+		private static List<Point> galaxiesInRow(List<Point> galaxies, long row) {
 			return galaxies.stream()
 					.filter(galaxy -> galaxy.y == row)
 					.toList();
 		}
 
-		private static List<Point> shiftColumns(List<Point> galaxies) {
+		private static List<Point> shiftColumns(List<Point> galaxies, long factor) {
 			var shiftedGalaxies = new ArrayList<Point>();
 
-			var shift = 0;
-			for (int column = 0; column < columns(galaxies); column++) {
+			var shift = 0L;
+			for (var column = 0; column < columns(galaxies); column++) {
 				var galaxiesInColumn = galaxiesInColumn(galaxies, column);
 				if (galaxiesInColumn.isEmpty()) {
-					shift++;
+					shift += factor;
 				} else {
 					for (var galaxy : galaxiesInColumn) {
 						shiftedGalaxies.add(new Point(galaxy.x + shift, galaxy.y));
@@ -112,14 +116,14 @@ final class Day11 {
 			return shiftedGalaxies;
 		}
 
-		private static int columns(List<Point> galaxies) {
+		private static long columns(List<Point> galaxies) {
 			return galaxies.stream()
-						   .mapToInt(Point::x)
+						   .mapToLong(Point::x)
 						   .max()
 						   .orElse(-1) + 1;
 		}
 
-		private static List<Point> galaxiesInColumn(List<Point> galaxies, int column) {
+		private static List<Point> galaxiesInColumn(List<Point> galaxies, long column) {
 			return galaxies.stream()
 					.filter(galaxy -> galaxy.x == column)
 					.toList();
--- 2023/src/test/java/de/mvitz/aoc2023/Day11Tests.java
+++ 2023/src/test/java/de/mvitz/aoc2023/Day11Tests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import static de.mvitz.aoc2023.Day11.sumOfAllShortestPathsBetweenGalaxyPairs;
+import static de.mvitz.aoc2023.Day11.sumOfAllShortestPathsBetweenOlderGalaxyPairs;
 import static de.mvitz.aoc2023.Utils.contentOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -31,5 +32,35 @@ class Day11Tests {
 		var solution = sumOfAllShortestPathsBetweenGalaxyPairs(input);
 
 		assertThat(solution, is(9_214_785L));
+	}
+
+	@Test
+	@Order(2)
+	void part2_example1() {
+		var input = contentOf("day11_example.txt");
+
+		var solution = sumOfAllShortestPathsBetweenOlderGalaxyPairs(input, 10);
+
+		assertThat(solution, is(1_030L));
+	}
+
+	@Test
+	@Order(3)
+	void part2_example2() {
+		var input = contentOf("day11_example.txt");
+
+		var solution = sumOfAllShortestPathsBetweenOlderGalaxyPairs(input, 100);
+
+		assertThat(solution, is(8_410L));
+	}
+
+	@Test
+	@Order(4)
+	void part2() {
+		var input = contentOf("day11.txt");
+
+		var solution = sumOfAllShortestPathsBetweenOlderGalaxyPairs(input, 1_000_000);
+
+		assertThat(solution, is(613_686_987_427L));
 	}
 }
