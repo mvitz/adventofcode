6723b23513043f988150ac22fe75141154871e3c
Michael Vitz <vitz.michael@googlemail.com>
2024-12-03 15:18:11 +0100

    2024: Day  3 - Refactoring

--- 2024/src/main/java/de/mvitz/aoc2024/day03/Day03.java
+++ 2024/src/main/java/de/mvitz/aoc2024/day03/Day03.java
@@ -21,28 +21,11 @@ public final class Day03 {
 
 	private static List<Pair<Long, Long>> multiplicationsFrom(String line) {
 		var multiplications = new ArrayList<Pair<Long, Long>>();
-
-		int start = 0, end = 0;
-		while (end < line.length()) {
-			start = line.indexOf("mul(", end);
-			if (start < 0) {
-				return multiplications;
-			}
-
-			end = line.indexOf(')', start);
-			if (end < 0) {
-				return multiplications;
-			}
-
-			var candidate = line.substring(start, end + 1);
-			var m = MUL_PATTERN.matcher(candidate);
-			if (m.matches()) {
-				multiplications.add(Pair.of(
-						Long.parseLong(m.group(1)),
-						Long.parseLong(m.group(2))));
-			} else {
-				end = start + 1;
-			}
+		var m = MUL_PATTERN.matcher(line);
+		while (m.find()) {
+			multiplications.add(Pair.of(
+					Long.parseLong(m.group(1)),
+					Long.parseLong(m.group(2))));
 		}
 		return multiplications;
 	}
