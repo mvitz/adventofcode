d272aafe3ff1ad8c65a88091c748a22a08258d37
Michael Vitz <vitz.michael@googlemail.com>
2024-12-03 21:35:16 +0100

    2024: Day  3 - Part  2

--- 2024/src/main/java/de/mvitz/aoc2024/day03/Day03.java
+++ 2024/src/main/java/de/mvitz/aoc2024/day03/Day03.java
@@ -1,32 +1,70 @@
 package de.mvitz.aoc2024.day03;
 
-import de.mvitz.aoc2024.utils.Pair;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public final class Day03 {
 
-	private static final Pattern MUL_PATTERN = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+	private static final Pattern MUL_PATTERN = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|(do(n't)*)\\(\\)");
+
+	private interface TokenCallback {
+		default void onMul(long first, long second) {
+		}
+
+		default void onDo() {
+		}
+
+		default void onDont() {
+		}
+	}
 
 	private Day03() {
 	}
 
 	public static long findSumOfValidMultiplicationsFrom(String input) {
-		return multiplicationsFrom(input).stream()
-				.mapToLong(pair -> pair.left() * pair.right())
-				.sum();
+		return parse(input, new TokenCallback() {
+			long result = 0;
+
+			@Override
+			public void onMul(long first, long second) {
+				result += first * second;
+			}
+		}).result;
 	}
 
-	private static List<Pair<Long, Long>> multiplicationsFrom(String line) {
-		var multiplications = new ArrayList<Pair<Long, Long>>();
-		var m = MUL_PATTERN.matcher(line);
+	public static long findSumOfValidEnabledMultiplicationsFrom(String input) {
+		return parse(input, new TokenCallback() {
+			boolean enabled = true;
+			long result = 0;
+
+			@Override
+			public void onDo() {
+				enabled = true;
+			}
+
+			@Override
+			public void onDont() {
+				enabled = false;
+			}
+
+			@Override
+			public void onMul(long first, long second) {
+				if (enabled) {
+					result += first * second;
+				}
+			}
+		}).result;
+	}
+
+	private static <T extends TokenCallback> T parse(String input, T callback) {
+		var m = MUL_PATTERN.matcher(input);
 		while (m.find()) {
-			multiplications.add(Pair.of(
-					Long.parseLong(m.group(1)),
-					Long.parseLong(m.group(2))));
+			switch (m.group()) {
+				case "do()" -> callback.onDo();
+				case "don't()" -> callback.onDont();
+				default ->
+						callback.onMul(Long.parseLong(m.group(1)), Long.parseLong((m.group(2))));
+			}
 		}
-		return multiplications;
+		return callback;
 	}
 }
--- 2024/src/test/java/de/mvitz/aoc2024/day03/Day03Tests.java
+++ 2024/src/test/java/de/mvitz/aoc2024/day03/Day03Tests.java
@@ -4,6 +4,7 @@ import de.mvitz.aoc2024.utils.Files;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import static de.mvitz.aoc2024.day03.Day03.findSumOfValidEnabledMultiplicationsFrom;
 import static de.mvitz.aoc2024.day03.Day03.findSumOfValidMultiplicationsFrom;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,4 +37,32 @@ class Day03Tests {
         assertThat(solution)
                 .isEqualTo(173_529_487);
     }
+
+	@Test
+	@Order(2)
+	void part2_example() {
+		// given
+		var input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+		// when
+		var solution = findSumOfValidEnabledMultiplicationsFrom(input);
+
+		// then
+		assertThat(solution)
+				.isEqualTo(48);
+	}
+
+	@Test
+	@Order(3)
+	void part2() {
+		// given
+		var input = Files.contentOf("day03/input.txt");
+
+		// when
+		var solution = findSumOfValidEnabledMultiplicationsFrom(input);
+
+		// then
+		assertThat(solution)
+				.isEqualTo(99_532_691);
+	}
 }
