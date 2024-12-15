3f02bb5ef1c8a8739d0d35214dae5a09e8a2401d
Michael Vitz <vitz.michael@googlemail.com>
2024-12-15 23:08:20 +0100

    2024: Refactoring

--- 2024/src/main/java/de/mvitz/aoc2024/Main.java
+++ 2024/src/main/java/de/mvitz/aoc2024/Main.java
@@ -2,21 +2,18 @@ package de.mvitz.aoc2024;
 
 import de.mvitz.aoc2024.utils.Files;
 
-import java.io.IO;
-
 import static de.mvitz.aoc2024.day05.Day05.sumOfMiddlePageNumbersFromCorrectlyOrderedUpdates;
 import static de.mvitz.aoc2024.day05.Day05.sumOfMiddlePageNumbersFromIncorrectlyOrderedUpdatesAfterOrdering;
 
 public class Main {
 
-	@SuppressWarnings("preview")
 	public static void main() {
 		var updatePlan = Files.contentOf("day05/input.txt");
 
 		var result = sumOfMiddlePageNumbersFromCorrectlyOrderedUpdates(updatePlan);
-		IO.println("Sum of middlepage numbers for correctly ordered pages is: " + result);
+		System.out.println("Sum of middlepage numbers for correctly ordered pages is: " + result);
 
 		result = sumOfMiddlePageNumbersFromIncorrectlyOrderedUpdatesAfterOrdering(updatePlan);
-		IO.println("Sum of middlepage numbers for incorrectly ordered pages is: " + result);
+		System.out.println("Sum of middlepage numbers for incorrectly ordered pages is: " + result);
 	}
 }
