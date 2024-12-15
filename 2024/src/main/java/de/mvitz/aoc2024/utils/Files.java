9e7b7656d49e6cb4ea4e25f58e796cdc619951ab
Michael Vitz <vitz.michael@googlemail.com>
2024-12-15 22:44:17 +0100

    2024: Refactoring

--- 2024/src/main/java/de/mvitz/aoc2024/utils/Files.java
+++ 2024/src/main/java/de/mvitz/aoc2024/utils/Files.java
@@ -1,10 +1,8 @@
 package de.mvitz.aoc2024.utils;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
 
-import static java.util.Objects.requireNonNull;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class Files {
 
@@ -13,10 +11,12 @@ public final class Files {
 
 	@SuppressWarnings("java:S112")
 	public static String contentOf(String name) {
-		try {
-			final var resource = requireNonNull(Files.class.getResource("/" + name)).toURI();
-			return java.nio.file.Files.readString(Path.of(resource));
-		} catch (URISyntaxException | IOException e) {
+		try (var in = Files.class.getResourceAsStream("/" + name)) {
+			if (in == null) {
+				throw new RuntimeException("Unable to find file: " + name);
+			}
+			return new String(in.readAllBytes(), UTF_8);
+		} catch (IOException e) {
 			throw new RuntimeException("Unable to read content of: " + name, e);
 		}
 	}
