255485185d3a64ecaacf6ac99f9385a8f5d4172d
Michael Vitz <vitz.michael@googlemail.com>
2024-12-17 22:38:07 +0100

    2024: Refactoring

--- 2024/src/main/java/de/mvitz/aoc2024/utils/Pair.java
+++ 2024/src/main/java/de/mvitz/aoc2024/utils/Pair.java
@@ -1,6 +1,16 @@
 package de.mvitz.aoc2024.utils;
 
+import java.util.function.Function;
+
 public record Pair<L, R>(L left, R right) {
+
+	public <T> Pair<T, R> mapLeft(Function<L, T> function) {
+		return new Pair<>(function.apply(left), right);
+	}
+
+	public <T> Pair<L, T> mapRight(Function<R, T> function) {
+		return new Pair<>(left, function.apply(right));
+	}
 
 	public static <L, R> Pair<L, R> of(L left, R right) {
 		return new Pair<>(left, right);
